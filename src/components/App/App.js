7fce236482e413978205935a0bfb6d3e7dfd4541
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-04-24 02:13:48 -0700

    Yelp React Project is started without fetching yet

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import './App.css';
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar/>
+      <BusinessList/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import "./SearchBar.css"
+
+export default class SearchBar extends Component {
+    
+        sortByOptions={
+            "Best Match": "best_match",
+            "Highest Rated": "rating",
+            "Most Reviewed": "review_count"
+        }
+        renderSortByOptions(){
+            return Object.keys(this.sortByOptions).map(sortByOption=>{
+                    const sortByOptionValue=this.sortByOptions[sortByOption]
+                    return <li key={sortByOptionValue}>{sortByOption}</li>
+            })
+        }
+    
+   
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a href='www.#.com'>Let's Go</a>
+        </div>
+      </div>
+
+        )
+    }
+}
